2e8ac19a90d5d837322446967e70aff7708b8d91
coper3976 <gssinjy1@naver.com>
2021-12-20 17:51:07 +0900

    init

--- crawl.js
+++ crawl.js
@@ -6,7 +6,7 @@ const url = async () => {
     try {
         return await axios.get('https://www.kasina.co.kr/goods/populate.php', {
             headers : {
-                'user-agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+                'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
                 'Accept-Charset': 'utf-8'      
             }
         });
@@ -25,13 +25,11 @@ const parsing = async () => {
         const brand = $(divs).find('.item_brand').text().trim();
         const productID = $(divs).find('.item_name').text().trim();
         const price = $(divs).find('.item_price').text().trim();
-        const imgLink = "https://www.kasina.co.kr" + $('#contents > div.sub_content > div.content > div > div.goods_list_cont.goods_content_7 > div > ul > li:nth-child(1) > div > div.item_photo_box > a > img').attr('src')
-        const productLink = "https://www.kasina.co.kr" + $('#contents > div.sub_content > div.content > div > div.goods_list_cont.goods_content_7 > div > ul > li:nth-child(1) > div > div.item_photo_box > a').attr('href').trim().replace('..', '')
+        const imgLink = "https://www.kasina.co.kr" + $('#contents > div.sub_content > div.content > div > div.goods_list_cont.goods_content_7 > div > ul > li:nth-child(1) > div > div.item_photo_box > a > img').attr('src');
+        const productLink = "https://www.kasina.co.kr" + $('#contents > div.sub_content > div.content > div > div.goods_list_cont.goods_content_7 > div > ul > li:nth-child(1) > div > div.item_photo_box > a').attr('href').trim().replace('..', '');
 
         console.log(brand, "//", productID, "//", price, "//", productLink, "//", imgLink);
     });
 }
 
 parsing();
-
-
