31c77004534e37f3576def672c7240f9a8d3cade
coper3976 <gssinjy1@naver.com>
2021-12-20 13:26:52 +0900

    init

--- crawl.js
+++ crawl.js
@@ -25,8 +25,10 @@ const parsing = async () => {
         const brand = $(divs).find('.item_brand').text().trim();
         const productID = $(divs).find('.item_name').text().trim();
         const price = $(divs).find('.item_price').text().trim();
+        const imgLink = "https://www.kasina.co.kr" + $('#contents > div.sub_content > div.content > div > div.goods_list_cont.goods_content_7 > div > ul > li:nth-child(1) > div > div.item_photo_box > a > img').attr('src')
+        const productLink = "https://www.kasina.co.kr" + $('#contents > div.sub_content > div.content > div > div.goods_list_cont.goods_content_7 > div > ul > li:nth-child(1) > div > div.item_photo_box > a').attr('href').trim().replace('..', '')
 
-        console.log(brand, "//", productID, "//", price);
+        console.log(brand, "//", productID, "//", price, "//", productLink, "//", imgLink);
     });
 }
 
